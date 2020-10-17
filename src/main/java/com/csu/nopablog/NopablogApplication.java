4eac60193bda6fc36e9985c1a467967bf8818ca5
nopachen <nopa607@163.com>
2020-10-17 20:06:20 +0800

    first commit

---
+++ src/main/java/com/csu/nopablog/NopablogApplication.java
@@ -0,0 +1,13 @@
+package com.csu.nopablog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NopablogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NopablogApplication.class, args);
+    }
+
+}
